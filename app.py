2fde948e620c7bb52f797985a1cb8663c46960a0
Rajveer Singh <139690830+rajveer-singh0@users.noreply.github.com>
2025-09-14 16:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def predict():
     return jsonify({'error': 'File type not allowed'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=False)
